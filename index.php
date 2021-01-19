31d4eeb7d75a20b18733a0c083782d79e2314167
fsrshipping <74324252+fsrshipping@users.noreply.github.com>
2021-01-19 16:45:15 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html>
+<head>
+                  
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Happy+Monkey&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">    
+    <title>A'HO</title>
+</head> 
+<body>
+    <div class="rainbow-bar"></div>
+    <div class="header">
+        <div class="header-layer">
+            
+            <h1 class="header-title">Sexuality and Freedom <br>
+                    African Rainbow Initiative</h1>     
+            <p class="subtitle"><br><br>African Homosexual Organisation</p>
+            <p class="header-text"><br><br>The quest for equal LGBTI rights around the world</p>
+
+            <nav>
+                <ul class="navigation">
+                    <li><a href="#mission" class="nav-link">Our mission</a></li>
+                    <li><a href="#support" class="nav-link">Support</a></li>
+                    <li><a href="#contact" class="nav-link">Contact us</a></li> 
+                    <!-- <li><a href="#me" class="nav-link">Look at me</a></li> -->
+                </ul>
+            </nav>
+
+            
+
+        </div> <!-- /.header-layer -->
+    </div> <!-- /.header -->
+
+    <!-- Picture section
+    <div class="rainbow-bar" id=""></div>
+
+    <div class="section-one">
+        <h2 class="section-one-title"></h2>
+        <h3 class="section-one-subtitle"></h3>
+        <p></p>
+        <div class="pictures-container">
+            <a class="picture-link" href="">
+                <img class="picture" src="" alt="">
+            </a>
+            <a class="picture-link" href="">
+                <img class="picture" src="" alt="">
+            </a>
+        </div>
+        <p></p>
+        
+        <a class="picture-large-link" href="">    
+            <img class="picture-large" src="" alt="">
+        </a>
+        <p>It takes some intelligence and insight to figure out you're gay and then a tremendous amount of balls to live it and live it proudly</p>
+
+    </div> -->
+
+    <div class="rainbow-bar" id="misson"></div>
+    
+    <div class="mission">
+        <div class="mission-header-section">
+            <div class="side-picture-container"><img src="img/goat.jpg" class="side-picture"></div>
+            <div class="mission-header-text">
+                <h2 class="mission-section-title">Our mission</h2>
+                <h3 class="mission-section-subtitle">Global rights for LGBTI's</h3>
+                <p class="mission-p">LGBTI rights are not a fact for everybody in this colorful world. We want that to change</p>
+                
+            </div> <!-- /.mission-header-text -->
+            <div class="side-picture-container"><img src="img/goat.jpg" class="side-picture side-picture-two"></div>
+        </div> <!-- /.mission-section -->
+        
+        <img class="picture-large" src="img/lads/danni-angel.jpg">
+
+        <div class="mission-section">
+            <div class="section-side-picture-container"><img src="img/lads/girls/girl-4.jpg" class="section-side-picture"></div>
+            <div class="mission-section-text">
+                <h3 class="mission-section-subtitle">A'HO</h3>
+                <p class="mission-p">We call ourselves African Homosexual organisation, a.k.a. A'HO and we are proud for being unique 
+                        and expressing ourselves. We are a small group of active A'HOs in Europe combining forces to make this world 
+                        a better place where everybody is accepted for who they are.</p>            
+            </div> <!-- /.mission-header-text -->
+            <div class="section-side-picture-container"><img src="img/lads/danni-football.jpg" class="section-side-picture"></div>
+        </div> <!-- /.mission-header -->
+
+        <div class="mission-section">
+            <div class="section-side-picture-container"><img src="img/lads/levis-sign.jpg" class="section-side-picture"></div>
+            <div class="mission-section-text">
+                <h3 class="mission-section-subtitle">Gays in Africa</h3>
+                <p class="mission-p">In many Western countries being A'HO is accepted and coming out of the closet is encouraged. 
+                        In many countries A'HO men and woman are suppressed and prosecuted. Our organisation targets especially A'HO man
+                        and woman in African countries.</p>
+            </div> <!-- /.mission-header-text -->
+            <div class="section-side-picture-container"><img src="img/lads/girls/girl-1.jpg" class="section-side-picture"></div>
+        </div> <!-- /.mission-header -->
+
+        <div class="mission-section">
+            <div class="section-side-picture-container"><img src="img/lads/cats-sign.jpg" class="section-side-picture"></div>
+            <div class="mission-section-text">
+                <h3 class="mission-section-subtitle">We help A'HOs</h3>
+                 <p class="mission-p">How can we help? First of all we are here for moral support but we know that it is not enough.
+                        We know that it's all about action. You want to be free to express yourself and show the world who you are. 
+                        That's why we support projects in Africa that are actively promoting A'HO rights in their country.</p>
+            </div> <!-- /.mission-header-text -->
+            <div class="section-side-picture-container"><img src="img/lads/raisin-tattoo.jpg" class="section-side-picture"></div>
+        </div> <!-- /.mission-header -->
+
+        <img class="picture-large" src="img/lads/billboard.jpg">
+        
+        <div class="mission-section">
+                <div class="section-side-picture-container"><img src="img/lads/girls/girl-5.jpg" class="section-side-picture"></div>
+                <div class="mission-section-text">
+                    <h3 class="mission-section-subtitle">A'HO Activists</h3>
+                     <p class="mission-p">There are already active A'HOs in Africa. Each promoting our 
+                         cause and fighting for rights for A'HOs in their own and unique way. 
+                     </p>
+                </div> <!-- /.mission-header-text -->
+                <div class="section-side-picture-container"><img src="img/lads/girls/girl-6.jpg" class="section-side-picture"></div>
+            </div> <!-- /.mission-header -->
+
+        <img class="picture-large" src="img/kiss-crop.jpg">
+        <img class="picture-large" src="img/lads/cats-wings.jpg">
+        
+    </div> <!-- /mission-->
+
+    <!-- support section -->
+    <div class="rainbow-bar" id="support"></div>
+
+    <div class="support-section">
+
+        <div class="mission-section">
+            <div class="section-side-picture-container"><img src="img/lads/girls/girl-3.jpg" class="section-side-picture"></div>
+            <div class="mission-section-text">
+                <h3 class="mission-section-subtitle">Support us</h3>
+                <p>Are you interested in this cause and want to donate money to support the A'HO rights in Africa projects? We
+                    are happy with any amount you have to spare.</p>
+                <p>For donations over €2500,- you will be guaranteed to have a 
+                    space on our boat during the Gay-Pride next year! This offer only stands as long as there are available places, so make sure to place your donations on time</p>
+                </div> <!-- /.mission-header-text -->
+            <div class="section-side-picture-container"><img src="img/lads/sven-heart.jpg" class="section-side-picture"></div>
+        </div> <!-- /.mission-header -->
+
+        <img class="picture-large" src="img/gay-pride.jpg" alt="Our boat at Amsterdam Gay pride">
+         
+        <div class="mission-section">
+            <div class="section-side-picture-container"><img src="img/lads/cats-kiss.jpg" class="section-side-picture"></div>
+            <div class="mission-section-text">
+                <p>IBAN: NL39ING078640964321<br>
+                Account holder name: A'HO<br>
+                BIC: INGBNL2AXXX<br>
+                Bank Address: Financial Plaza Bijlmerstraat 107, 1108KW Amsterdam, Netherlands
+                </p>
+            </div>
+            <div class="section-side-picture-container"><img src="img/lads/sven-glitter.jpg" class="section-side-picture"></div>
+        </div>
+    </div>
+    
+    <!--contact us -->
+    <div class="rainbow-bar" id="contact"></div>
+
+    <div class="contact-section">
+        <div class="mission-section">
+            <div class="section-side-picture-container"><img src="img/lads/benny-sign.jpg" class="section-side-picture"></div>
+            <div class="mission-section-text">
+                <h2 class="contact-section-title">Contact us</h2>
+                <h3 class="contact-section-subtitle">If you are A'HO and need support, please contact us</h3>
+                <p>If you are not in Africa, but do need help, don't hesitate to reach out.<br><br></p>
+                <p>You can find us at</p>
+                <p>Waterlandpleinbuurt 28<br> 
+                    1027 EA Amsterdam</p>
+            </div> <!-- /.mission-header-text -->
+            <div class="section-side-picture-container"><img src="img/lads/sven-wings.jpg" class="section-side-picture"></div>
+        </div> <!-- /.mission-header -->
+
+        <div class="mission-section">
+            <div class="section-side-picture-container"><img src="img/lads/benny-wings.jpg" class="section-side-picture"></div>
+            <div class="mission-section-text">
+                <p>All A'HOs working with us do this in their spare time and have regular day-jobs. The best way to contact us is through email.<br>
+                    When you message us, please mention in the subject field </p>
+                <p>"I am A'HO and need support"</p> 
+                
+            </div>
+            <div class="section-side-picture-container"><img src="img/lads/girls/girl-2.jpg" class="section-side-picture"></div>
+        </div>    
+        <p> 
+            <btn id="contact-email" class="btn-email">Email us</btn>
+        </p>
+        
+        
+        <img class="picture-large" src="img/lads/femi.jpg" alt="Our boat at Amsterdam Gay pride">
+        
+    </div>
+
+
+    <div class="rainbow-bar"></div>
+    <script type="text/javascript" src="script/script.js"></script>
+</body>
+</html>
