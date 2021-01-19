0563b47e671481d7ae29a10d70d034e1970a1345
fsrshipping <74324252+fsrshipping@users.noreply.github.com>
2021-01-19 16:44:37 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const mailbutton = document.getElementById("contact-email");
+
+mailbutton.addEventListener("click", notify);
+
+function notify() {
+    alert("Your browser doesn't support this function, please contact your referrant");
+}
